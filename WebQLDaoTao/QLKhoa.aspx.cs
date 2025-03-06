eea9d4620bba5246e907acee8a67af6dd0bd1f65
ADMIN <ADMIN@PC233>
2025-03-06 12:58:35 +0700

    Add project files.

---
+++ WebQLDaoTao/QLKhoa.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebQLDaoTao.Models;
+namespace WebQLDaoTao
+{
+    public partial class QLKhoa : System.Web.UI.Page
+    {
+        KhoaDAO khDAO = new KhoaDAO();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //lien ket du lieu cho gvKhoa
+                gvKhoa.DataSource = khDAO.getAll();
+                gvKhoa.DataBind();
+            }
+
+        }
+    }
+}
